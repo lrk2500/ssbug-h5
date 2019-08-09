1e6ed47b637024c799c03ecb70b51d3ee2ff6873
lrk2500 <2500908911@qq.com>
2019-08-09 11:00:57 +0800

    first commit 11:00

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HomePage from '@/components/HomePage'
-import ArticleDisplay from '@/components/ArticleDisplay'
-import Login from '@/components/user/Login'
-import Register from '@/components/user/Register'
-import Release from '@/components/article/Release'
-import Writings from '@/components/article/Writings'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [{
+  routes: [
+		{
+			path: "*",
+			redirect: "/HomePage"
+		},
+		{
       path: '/homepage',
       name: 'HomePage',
       component: HomePage,
       children: [{
           path: 'writings',
           name: 'Writings',
-          component: Writings
+          // component: Writings,
+					component: resolve => require(['@/components/article/Writings'], resolve),
         }
 
       ]
@@ -25,22 +26,27 @@ export default new Router({
     {
       path: '/articleDisplay',
       name: 'ArticleDisplay',
-      component: ArticleDisplay
+      // component: ArticleDisplay
+			component: resolve => require(['@/components/ArticleDisplay'], resolve),
     },
     {
       path: '/login',
       name: 'Login',
-      component: Login
+      // component: Login,
+			component: resolve => require(['@/components/user/Login'], resolve),
     },
     {
       path: '/register',
       name: 'Register',
-      component: Register
+      // component: Register,
+			component: resolve => require(['@/components/user/Register'], resolve),
     },
     {
       path: '/release',
       name: 'Release',
-      component: Release
-    }
+      // component: Release
+			component: resolve => require(['@/components/article/Release'], resolve),
+    },
+
   ]
 })
